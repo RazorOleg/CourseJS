37d53d1bc605af8b796c51db7c3f760c6a38ae00
RazorOleg <razno.oleg@gmail.com>
2021-10-02 23:57:24 +0300

    createdFirstCommit

---
+++ alert.js
@@ -0,0 +1,39 @@
+alert('Я JavaScript');
+
+/*let admin;
+let name;
+
+name = "Джон";
+
+admin = name;
+alert(`Имя: ${admin}`);
+
+let newName = "Oleg";
+
+alert( `hello ${1}` );
+
+alert( `hello ${"newName"}` );
+
+alert( `hello ${newName}` );*/
+
+let userName = prompt("Как Вас зовут?", 'Имя');
+alert(`Ваше имя: ${userName}`);
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b); // 12
+
+let message = prompt("Какое «официальное» название JavaScript?", "");
+
+/*if(message=="ECMAScript")
+{
+    alert("Верно!");
+}
+else
+{
+    alert("Не знаете? ECMAScript!");
+}*/
+
+
+

e25e70ba0c9a645d7dac562cd7bbb3c1bbed1c7d
RazorOleg <razno.oleg@gmail.com>
2021-10-13 00:56:21 +0300

    PR changes

--- alert.js
+++ alert.js
@@ -1,89 +1,5 @@
 alert('Я JavaScript');
 
-/*function clone(object) {
-    let newObject = {}
-
-    for(let prop in object) {
-        newObject[prop] = (typeof object[prop] !== 'object') ? object[prop] : clone(object[prop])
-    }
-    return newObject
-}
-
-let calculator = {
-    read() {
-        this.val1 = +prompt("Введите первое значение: ");
-        this.val2 = +prompt("Введите второе значение: ");
-    },
-    sum() {
-        let sum = this.val1 + this.val2;
-        return sum;
-
-    },
-
-    mul() {
-        let mul = this.val1 * this.val2;
-        return mul;
-
-    }
-};
-
-calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
-
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this;
-    },
-    down() {
-        this.step--;
-        return this;
-    },
-    showStep: function() { // показывает текущую ступеньку
-        alert( this.step );
-        return this;
-    }
-};
-
-ladder.up().up().down().showStep();*/
-
-/*let obj = {};
-
-function A() {return obj; }
-
-function B() {return obj; }
-
-let a = new A;
-let b = new B;
-
-alert( a == b );*/
-
-/*function Calculator() {
-    this.read = function () {
-        this.val1 = +prompt("Введите первое значение: ");
-        this.val2 = +prompt("Введите второе значение: ");
-    },
-    this.sum = function () {
-        let sum = this.val1 + this.val2;
-        return sum;
-
-    },
-
-    this.mul = function () {
-        let mul = this.val1 * this.val2;
-        return mul;
-    }
-
-}*/
-
-/*let calculator = new Calculator();
-calculator.read();
-
-alert( "Sum=" + calculator.sum() );
-alert( "Mul=" + calculator.mul() );*/
-
 function Accumulator(startingValue) {
     this.value = startingValue;
 
@@ -101,3 +17,34 @@ accumulator.read();
 accumulator.read();
 
 alert(accumulator.value);
+
+class Soldier {
+    constructor(value) {
+        this.value = value;
+        }
+    Shoot() {
+        return "выстрел";
+    }
+    Heal() {
+        return "лечение";
+    }
+    Run() {
+        return "побег";
+    }
+
+    get condition() {
+        if(this.value === "солдат здоров") {
+            return this.Shoot();
+        } else if(this.value === "солдат ранен") {
+            return this.Heal();
+        } else if(this.value === "солдат проигрывает") {
+            return this.Run();
+        } else {
+            return;
+        }
+    }
+}
+
+let value = prompt("Введите значения:", "солдат здоров");
+let res = new Soldier(value);
+console.log(res.condition);

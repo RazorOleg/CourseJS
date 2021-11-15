6393be4add8b8dfdf844e4d3da6bac7c1b351352
RazorOleg <91757681+RazorOleg@users.noreply.github.com>
2021-11-16 00:29:17 +0200

    Prototype&Inheritance (#6)

--- alert.js
+++ alert.js
@@ -1,112 +1,50 @@
+alert('Я JavaScript');
 
-function sumTo(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++ ) {
-        sum += i;
-    }
-    return sum;
-}
+function Accumulator(startingValue) {
+    this.value = startingValue;
 
-console.log(sumTo(4));
-
-function sumTo1(n) {
-    if (n == 1) {
-        return n;
-    } else {
-        return n + sumTo(n-1);
+    this.read = function () {
+        this.number = +prompt("Введите число: ");
+        this.value += this.number;
+        console.log(this.value);
     }
 }
 
-console.log(sumTo1(100));
+let accumulator = new Accumulator(1);
 
-function sumTo2(n) {
-    let sum = 0;
-    sum = ((2 + ( n-1 ))/2) * n;
-    return sum;
-}
+accumulator.read();
 
-console.log(sumTo2(4));
+accumulator.read();
 
-function factorial(n) {
-    if (n == 1) {
-        return n;
-    } else {
-        return n * factorial(n-1);
+alert(accumulator.value);
+
+class Soldier {
+    constructor(value) {
+        this.value = value;
+        }
+    Shoot() {
+        return "выстрел";
     }
-}
-
-console.log(factorial(3));
-
-function fib(n) {
-    if (n == 1 || n ==2) {
-        return 1;
-    } else {
-        return fib(n - 1) + fib(n - 2);
+    Heal() {
+        return "лечение";
     }
-}
+    Run() {
+        return "побег";
+    }
 
-console.log(fib(7));
-
-let list = {
-    value: 1,
-    next: {
-        value: 2,
-        next: {
-            value: 3,
-            next: {
-                value: 4,
-                next: null
-            }
+    get condition() {
+        if(this.value === "солдат здоров") {
+            return this.Shoot();
+        } else if(this.value === "солдат ранен") {
+            return this.Heal();
+        } else if(this.value === "солдат проигрывает") {
+            return this.Run();
+        } else {
+            return;
         }
     }
-};
-
-function printList(list) {
-    let newList = list;
-
-    while (newList) {
-        console.log(newList.value);
-        newList = newList.next;
-    }
 }
 
-printList(list);
-
-function printList1(list) {
-    console.log(list.value);
-
-    if (list.next) {
-        printList(list.next);
-    }
-}
-
-printList1(list);
-
-
-function printReverseList(list) {
-    let newList = list;
-    let arr = [];
-
-    while (newList) {
-        arr.push(newList.value);
-        newList = newList.next;
-    }
-
-    for (let i = arr.length - 1; i >= 0; i-- ) {
-        console.log(arr[i])
-    }
-
-}
-
-printReverseList(list);
-
-function printReverseList1(list) {
-
-    if (list.next) {
-        printReverseList1(list.next);
-    }
-
-    console.log(list.value);
-}
-
-printReverseList1(list);
+let value = prompt("Введите значения:", "солдат здоров");
+let res = new Soldier(value);
+console.log(res.condition);

d76304537eaaba94a537cbe25a674f9d1613330a
RazorOleg <razno.oleg@gmail.com>
2022-01-11 23:01:48 +0200

    Classes

--- alert.js
+++ alert.js
@@ -1,50 +1,88 @@
-alert('Я JavaScript');
 
-function Accumulator(startingValue) {
-    this.value = startingValue;
+//9.1
+class Clock {
 
-    this.read = function () {
-        this.number = +prompt("Введите число: ");
-        this.value += this.number;
-        console.log(this.value);
+    constructor({template}) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render, 1000);
     }
 }
 
-let accumulator = new Accumulator(1);
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
 
-accumulator.read();
 
-accumulator.read();
+//9.2
+class Animal {
 
-alert(accumulator.value);
-
-class Soldier {
-    constructor(value) {
-        this.value = value;
-        }
-    Shoot() {
-        return "выстрел";
-    }
-    Heal() {
-        return "лечение";
-    }
-    Run() {
-        return "побег";
+    constructor(name) {
+        this.name = name;
     }
 
-    get condition() {
-        if(this.value === "солдат здоров") {
-            return this.Shoot();
-        } else if(this.value === "солдат ранен") {
-            return this.Heal();
-        } else if(this.value === "солдат проигрывает") {
-            return this.Run();
-        } else {
-            return;
-        }
+}
+
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name);
+        this.created = Date.now();
     }
 }
 
-let value = prompt("Введите значения:", "солдат здоров");
-let res = new Soldier(value);
-console.log(res.condition);
+let rabbit = new Rabbit("Белый кролик");
+alert(rabbit.name);
+
+class ExtendedClock extends Clock {
+    constructor(props) {
+        super(props);
+        let { precision=1000 } = props;
+        this.precision = precision;
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render, this.precision)
+    }
+}
+
+let extendsClock = new ExtendedClock({template: 'h:m:s'});
+extendsClock.start();
+
+//9.3-9.7
+class Rabbit2 extends Object {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+}
+
+let rabbit2 = new Rabbit2("Кроль");
+
+alert( rabbit2.hasOwnProperty('name') );

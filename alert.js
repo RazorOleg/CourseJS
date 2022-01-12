72967e1d198ad48f4aa26ee659390b1ca2404332
RazorOleg <91757681+RazorOleg@users.noreply.github.com>
2022-01-12 22:10:38 +0200

    Hot fix (#9)
    * FixOverwritedData
    
    * FixOverwritedData
    
    * FixData

--- alert.js
+++ alert.js
@@ -1,6 +1,121 @@
+//8.1-8.4 Prototype & Inheritance
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3,
+    __proto__: head
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+};
+
+let pockets = {
+    money: 2000,
+    __proto__: bed
+};
+
+alert(pockets.pen);
+alert(bed.glasses);
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {
+    stomach: [],
+    __proto__: hamster
+};
+
+let lazy = {
+    stomach: [],
+    __proto__: hamster
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+speedy.eat("orange");
+alert( speedy.stomach ); // apple
+
+lazy.eat("orange");
+alert( lazy.stomach );
+
+
+/*function Rabbit(name) {
+    this.name = name;
+    alert(name);
+}
+
+let obj = new Rabbit("White Rabbit");
+
+let obj2 = new obj.constructor("Black Rabbit");*/
+
+function Rabbit(name) {
+    this.name = name;
+    alert(name);
+}
+Rabbit.prototype = { };
+
+let obj = new Rabbit("White Rabbit");
+
+let obj2 = new obj.constructor("Black Rabbit");
+
+alert(obj2.name);
+
+
+/*Function.prototype.defer = function (ms) {
+    setTimeout(this, ms)
+}
+
+function f() {
+    alert("Hello!");
+}
+
+f.defer(1000);*/
+
+
+Function.prototype.defer = function(ms) {
+    let f = this;
+    return function(...args) {
+        setTimeout(() => f.apply(this, args), ms);
+    }
+};
+
+function f(a, b) {
+    alert( a + b );
+}
+
+f.defer(1000)(1, 2);
+
+
+let dictionary = Object.create(null, {
+    toString: {
+        value() {
+            return Object.keys(this).join();
+        }
+    }
+});
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test";
+
+for(let key in dictionary) {
+    alert(key);
+}
+
+alert(dictionary);
+
+
 //9.1
 class Clock {
-
     constructor({template}) {
         this.template = template;
     }
@@ -34,10 +149,9 @@ class Clock {
         this.timer = setInterval(() => this.render, 1000);
     }
 
-
+}
 //9.2
-class Animal {
-
+    class Animal {
     constructor(name) {
         this.name = name;
     }
@@ -81,3 +195,5 @@ class Rabbit2 extends Object {
 let rabbit2 = new Rabbit2("Кроль");
 
 alert( rabbit2.hasOwnProperty('name') );
+console.log("Hello");
+console.log("Hello");

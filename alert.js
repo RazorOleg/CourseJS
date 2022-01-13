93b11ce189e735db79936d698d032d49cb4449c9
RazorOleg <razno.oleg@gmail.com>
2022-01-13 22:07:55 +0200

    NewFix

--- alert.js
+++ alert.js
@@ -197,3 +197,34 @@ alert( err.stack ); // stack
 
 alert( err instanceof FormatError ); // true
 alert( err instanceof SyntaxError );
+
+//11.1-11.4
+function delay(ms) {
+    let promise = new Promise(resolve => {
+        setTimeout(() => resolve("done"), ms);
+    });
+    return promise;
+}
+
+delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+console.log("Start");
+
+function hello() {
+    console.log("Hello");
+}
+
+setTimeout(hello, 0);
+
+hello();
+
+console.log("End");
+
+
+let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        throw new Error("Whoops!");
+    }, 1000);
+}).catch(alert);
+
+promise.catch(alert);

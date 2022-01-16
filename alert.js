8aeb40c559edf4954befcab58b6e6d2cc3ff02d9
RazorOleg <razno.oleg@gmail.com>
2022-01-16 21:26:55 +0200

    NewFix

--- alert.js
+++ alert.js
@@ -295,3 +295,17 @@ function f(){
 }
 
 f();
+
+//12.1-12.2
+function* pseudoRandom(seed) {
+    let result = seed;
+    while(true) {
+        yield result = result * 16807 % 2147483647;
+    }
+}
+
+let generator = pseudoRandom(1);
+
+alert(generator.next().value);
+alert(generator.next().value);
+alert(generator.next().value);
